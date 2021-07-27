5cface1ccef123750fd2178d88d3a0aaa3307680
Liam Egan <liam@wethecollective.com>
2021-07-27 13:54:35 -0700

    Updating frag recipe to include the shaders inline

--- src/recipes/FragmentShader/index.ts
+++ src/recipes/FragmentShader/index.ts
@@ -13,8 +13,33 @@ import { Triangle } from '../../geometry/Triangle'
 import { Uniform } from '../../core/Uniform'
 import { Vec2 } from 'wtc-math'
 
-const defaultShaderV = require('./vertex.gl')
-const defaultShaderF = require('./fragment.gl')
+const defaultShaderV = `
+attribute vec3 position;
+attribute vec2 uv;
+
+varying vec2 v_uv;
+
+void main() {
+gl_Position = vec4(position, 1.0);
+v_uv = uv;
+}
+`
+const defaultShaderF = `
+precision highp float;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec2 u_mouse;
+uniform sampler2D s_noise;
+
+uniform sampler2D b_noise;
+
+varying vec2 v_uv;
+
+void main() {
+gl_FragColor = vec4(vec3(cos(length(v_uv+u_time))*.5+.5, sin(v_uv+u_time)*.5+.5),1);
+}
+`
 
 interface WTCGLUniformArray {
   [index: string]: Uniform
