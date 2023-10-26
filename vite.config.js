b8291e8015fcc05cb72b5efcb9fb961334933bb8
Marlon Marcello <marlon.marcello@gmail.com>
2023-10-26 16:43:22 -0700

    fix: bundle types

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,36 @@
-import { defineConfig } from "vite"
-import { resolve } from "path"
-import glsl from "vite-plugin-glsl"
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import glsl from 'vite-plugin-glsl'
+import dts from 'vite-plugin-dts'
+
+// eslint-disable-next-line no-undef
+const resolvePath = (str) => resolve(__dirname, str)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl(),
+    dts({
+      include: 'src/lib/**/*.ts'
+    })
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      // eslint-disable-next-line no-undef
-      entry: resolve(__dirname, "src/lib/index.ts"),
-      name: "WTCGL",
+      entry: resolvePath('src/lib/index.ts'),
+      name: 'WTCGL'
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["wtc-math"],
+      external: ['wtc-math'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          'wtc-math': "wtcMath",
-        },
-      },
-    },
-  },
+          'wtc-math': 'wtcMath'
+        }
+      }
+    }
+  }
 })
